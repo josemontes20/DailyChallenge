21d66ce6108896e571e7029f3189c4ae0e0c54a0
José Montes <Jos Montes@172.16.49.220>
2015-03-06 14:54:04 +0100

    José: Verbesserungen und Kommentieren der UserControl-Bean

--- DailyChallenge/DailyChallenge/DailyChallenge-ejb/src/java/Controller/UserControl.java
+++ DailyChallenge/DailyChallenge/DailyChallenge-ejb/src/java/Controller/UserControl.java
@@ -10,6 +10,8 @@ import model.Anwender;
 
 /**
  * @author José Montes
+ * 
+ * Diese Bean ist für das Registrieren, Einloggen und Abmelden von Anwendern zuständig.
  */
 
 @Stateless
@@ -19,6 +21,7 @@ public class UserControl{
     @PersistenceContext (name = "DailyDB")
     private EntityManager em;
     
+    /*Diese Methode ist für das Anlegen eines Anwenders in der Anwender-Tabelle verantwortlich.*/
     public Anwender createUser(String username, String userpassword, String email, int score){
        
         em.setFlushMode(FlushModeType.AUTO);   
@@ -30,6 +33,7 @@ public class UserControl{
         
     }
     
+    /*Mithilfe dieser Methode wird überprüft, ob die eingegebenen Anmeldedaten in der Anwender-Twbelle vorhanden sind.*/
     public Anwender loginUser(String username, String userpassword){
         
         if(username == null || userpassword == null || username.isEmpty() || userpassword.isEmpty()){
@@ -47,41 +51,57 @@ public class UserControl{
         }
     }
     
+    
+    /*Diese Methode überprüft sämtliche Eingaben die der Anwender bei der Registrierung eingibt.
+    Dabei werden sämtliche Designanforderung der [BA 1/SA 1] umgesetzt.
+    
+    Das eingegebene Passwort und die geforderte Wiederholung desPassworts dürfen nicht leer sein.
+        Weiterhin müssen beide eingegebnen Passwörter übereinstimmen.
+    
+    Bei der Registrierung darf das Username-Feld nicht leer sein.
+        In der inneren IF-ELSE-Verzweigung wird überprüft, ob der eingegebene Username in der Anwender-Tabelle vorhanden ist.
+    
+    Bei der Registrierung muss die eingebene Email validiert werden. Hierfür wird die checkMailPattern-Methode aufgerufen.
+        Bei valider Eingaben wird weiterhin überprüft, ob die EMail in der Anwender-Tabelle bereits vorhanden ist.
+    */
+    
     public String registerValide(String username, String email, String passwort, String passwordRepeat){
-        if ((passwort != null && passwordRepeat != null) && passwort.equals(passwordRepeat) && (!passwort.equals(""))){
-           
-            if (username != null && (!username.equals(""))){
+        
+        if (passwort.isEmpty() || passwordRepeat.isEmpty() || (!(passwort.equals(passwordRepeat))) ){
+            return "PASSWORD NOT OK";
+        }
+        
+        if ((!username.isEmpty())){
                TypedQuery<Anwender> abfrageUser = em.createNamedQuery("Anwender.findByUser", Anwender.class)
                                               .setParameter("username", username);
                
-               if (abfrageUser.getResultList().isEmpty()){
-                   
-                   if(checkMailPattern(email)){
-                       TypedQuery<Anwender> abfrageEmail = em.createNamedQuery("Anwender.GetEmail", Anwender.class)
-                                              .setParameter("email", email);
-                       if(abfrageEmail.getResultList().isEmpty()){
-                           return "OK";
-                       }else{
-                           return "EMAIL REPEAT";
-                       } 
-                   }else{
-                       return "EMAIL NOT OK";
-                   }
-               }else{
+               if (!(abfrageUser.getResultList().isEmpty())){
                    return "USERNAME REPEAT";
                }
-           }else{
-               return "USERNAME NOT NULL";
-           }
+               
         }else{
-            return "PASSWORD NOT OK";
+            return "USERNAME NOT NULL";
         }
+
+        if(checkMailPattern(email)){
+            TypedQuery<Anwender> abfrageEmail = em.createNamedQuery("Anwender.GetEmail", Anwender.class)
+                                                .setParameter("email", email);
+            
+                if(abfrageEmail.getResultList().isEmpty()){
+                    return "OK";
+                }
+                else{
+                    return "EMAIL REPEAT";
+                } 
+        }else{
+             return "EMAIL NOT OK";
+        }
+                
     }
     
-    /** Überprüfung des Mail-Formats. */
-    protected boolean checkMailPattern(String login)
-    {
+    /** Überprüfung des Mail-Formats mithilfe eines endlichen Automats.*/
+    protected boolean checkMailPattern(String email){
         String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
-        return login.matches(EMAIL_REGEX);
+        return email.matches(EMAIL_REGEX);
     }
 }
