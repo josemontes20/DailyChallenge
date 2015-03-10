a2834fcd1be6a84a22e99d562aaf184a1833538f
Poloczek <Poloczek@zkv004-n01.marksys.de>
2015-03-10 16:49:22 +0100

    Simon: KategorieBean erstellt, um Kategorien erstellen zu können.

---
+++ DailyChallenge/DailyChallenge/DailyChallenge-ejb/src/java/Controller/KategorieBean.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import model.Kategorie;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Poloczek
+ */
+@Stateless
+@LocalBean
+public class KategorieBean {
+
+    @PersistenceContext (name = "DailyDB")
+    private EntityManager em;
+    
+    public Kategorie createKategorie(String beschreibung){
+        Kategorie k = new Kategorie(beschreibung);
+        em.setFlushMode(FlushModeType.AUTO);
+        em.persist(k);
+        k = em.merge(k);
+        em.flush();
+        return k;
+    }
+    
+}
