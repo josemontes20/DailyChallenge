53e3a03318ccf2e774c8e912529aec698b3371fc
José Montes <Jos Montes@172.16.49.220>
2015-03-24 11:48:25 +0100

    José: Build.xml geändert

--- DailyChallenge/DailyChallenge/DailyChallenge-ejb/test/daily/test/beans/Test_AnwenderBean.java
+++ DailyChallenge/DailyChallenge/DailyChallenge-ejb/test/daily/test/beans/Test_AnwenderBean.java
@@ -1,15 +1,21 @@
 package daily.test.beans;
 
 import daily.beans.AnwenderBean;
+import daily.model.Anwender;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 
 /*
     Diese Testklasse testet die Methoden der AnwenderBean.
 */
 public class Test_AnwenderBean {
+    static AnwenderBean anwBean;
     
-    public Test_AnwenderBean() {
+    @BeforeClass
+    public static void initTest(){
+        // Test-Kandidat
+        anwBean = new AnwenderBean();
     }
     
     /**
@@ -19,8 +25,6 @@ public class Test_AnwenderBean {
     public void testCheckMailPattern() throws Exception
     {
         System.out.println("Überprüfung der E-Mail-Validierungs-Methode!(AnwenderBean)");
-        // Test-Kandidat
-        AnwenderBean anwBean = new AnwenderBean();
         
         // Test Eingabe_1, Erwarteter Rückgabewert = true
         String mail1 = "test@test.com";
@@ -46,4 +50,32 @@ public class Test_AnwenderBean {
         System.out.println("Überprüfung der E-Mail-Validierungs-Methode abgeschlossen!");
     }
     
+    
+    /**
+     * Test zur Überprüfung des korrekten Verhaltens der validateRegistration()-Methode.
+     */
+    @Test
+    public void testvalidateRegistration() throws Exception{
+        
+        //Test Eingabe_1, Erwarteter Wert: USERNAME NOT NULL
+        String user_1 = "";
+        String email_1="test@test.de";
+        String password_1 = "test";
+        String password_2 = "test";
+        
+        String expResult1 = "USERNAME NOT NULL";
+        String result1 = anwBean.validateRegistration(user_1, email_1, password_1, password_2);
+        assertEquals(expResult1, result1);
+        
+        //Test Eingabe_2, Erwarteter Wert: PASSWORD NOT OK
+        String user_2="Test";
+        String email_2="test@test.de";
+        String password_3 = "test";
+        String password_4 = "123";
+        
+        String expResult_2 = "PASSWORD NOT OK";
+        String result_2 = anwBean.validateRegistration(user_2, email_2, password_3, password_4);
+        assertEquals(expResult_2, result_2); 
+        
+    }
 }
