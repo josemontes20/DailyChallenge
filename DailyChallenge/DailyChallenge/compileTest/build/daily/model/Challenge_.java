bbd42195fae493ddac0dd8baa2bd7fbc2f024e7b
José Montes <Jos Montes@172.16.49.220>
2015-03-24 11:55:17 +0100

    José: Build.xml geändert

---
+++ DailyChallenge/DailyChallenge/compileTest/build/daily/model/Challenge_.java
@@ -0,0 +1,17 @@
+package daily.model;
+
+import daily.model.Kategorie;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-03-24T11:39:15")
+@StaticMetamodel(Challenge.class)
+public class Challenge_ { 
+
+    public static volatile SingularAttribute<Challenge, String> aktivAmDatum;
+    public static volatile SingularAttribute<Challenge, Kategorie> kategorie;
+    public static volatile SingularAttribute<Challenge, Long> id;
+    public static volatile SingularAttribute<Challenge, String> beschreibung;
+
+}
---
+++ DailyChallenge/DailyChallenge/compileTest/build/daily/model/Kategorie_.java
@@ -0,0 +1,19 @@
+package daily.model;
+
+import daily.model.Anwender;
+import daily.model.Challenge;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-03-24T11:39:15")
+@StaticMetamodel(Kategorie.class)
+public class Kategorie_ { 
+
+    public static volatile ListAttribute<Kategorie, Challenge> challenges;
+    public static volatile SingularAttribute<Kategorie, String> name;
+    public static volatile SingularAttribute<Kategorie, Long> id;
+    public static volatile ListAttribute<Kategorie, Anwender> anwender;
+
+}
